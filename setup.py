e2d152bda2f618e78f751695b5376b5195e87f22
Phenicle <pheniclebeefheart@gmail.com>
2018-01-29 10:44:36 -0500

    updates to make pypl happy

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
    version='1.0.1',
    description='A simple, flexible, and powerful config reader for Python',
    license="Mozilla Public License 2.0 (MPL 2.0)",
-   long_description='A simple, flexible, and powerful config reader for Python. Features include support for multiple formats (ini, xml, json, yaml), layering (default, environment, local), and load modes (implicit, explicit)'
+   long_description='A simple, flexible, and powerful config reader for Python. Features include support for multiple formats (ini, xml, json, yaml), layering (default, environment, local), and load modes (implicit, explicit)',
    author='Phenicle',
    author_email='pheniclebeefheart@gmail.com',
    url="https://github.com/phenicle/cfgpy",
    packages=['cfgpy',],  #same as name
-   install_requires=['configparser,xmltodict'], #external packages as dependencies
+   install_requires=['configparser', 'xmltodict'], #external packages as dependencies
 )
